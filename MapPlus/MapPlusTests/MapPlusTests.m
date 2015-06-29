393c2da5c21016c9b27fceff3ffdc6189a204a53
Fletcher Woodruff <fletcherw@fb.com>
2015-06-29 11:19:07 -0700

    Instantiating xcode project and adding gitignore

---
+++ MapPlus/MapPlusTests/MapPlusTests.m
@@ -0,0 +1,40 @@
+//
+//  MapPlusTests.m
+//  MapPlusTests
+//
+//  Created by Fletcher Woodruff on 6/29/15.
+//  Copyright (c) 2015 MapPlusTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MapPlusTests : XCTestCase
+
+@end
+
+@implementation MapPlusTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
